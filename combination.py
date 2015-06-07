a81ee7ccf554c3f4b7baf7b8aa79e3a3ee79d049
jin ho Park <pjh0347@yahoo.co.kr>
2015-06-07 22:09:45 +0900

    update combination

--- combination.py
+++ combination.py
@@ -1,12 +1,52 @@
 # coding: utf8
+'''
+combination implementation
+'''
 
-# combination implementation by nested for loop
+import time
+import itertools
 
-n = 5
+def forLoop():
+	l = [1, 2, 3, 4, 5]
+	output = []
+	for i in range(0, len(l)):
+		for j in range(i+1, len(l)):
+			output.append( (l[i], l[j]) )
+	print output
 
-for i in range(0, n):
-	for j in range(i+1, n):
-		for k in range(j+1, n):
-			for l in range(k+1, n):
-				print i, j, k, l
+def recursion():
+	l = [1, 2, 3, 4, 5]
+	n = 2
+	def combination(l, n):
+		if n == 0:
+			return [[]]
+		r = []
+		for i, v in enumerate(l):
+			for j in combination(l[i+1:], n-1):
+				r.append([v]+j)
+		#print '\t' * n, n, 'of', l, '=', r
+		return r
+	print combination(l, n)
+
+def itertool():
+	l = [1, 2, 3, 4, 5]
+	n = 2
+	print list(itertools.combinations(l, n))
+
+if __name__ == '__main__':
+
+	s = time.time()
+	forLoop()
+	e = time.time()
+	print "elapsed : ", e-s
+
+	s = time.time()
+	recursion()
+	e = time.time()
+	print "elapsed : ", e-s
+
+	s = time.time()
+	itertool()
+	e = time.time()
+	print "elapsed : ", e-s
 
