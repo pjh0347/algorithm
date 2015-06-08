3ecbbcc27bf662d8dd5ad9d1f79dbc40a109f2af
jin ho Park <pjh0347@yahoo.co.kr>
2015-06-08 23:24:13 +0900

    update combination

--- combination.py
+++ combination.py
@@ -1,53 +1,54 @@
 # coding: utf8
 
 '''
-combination implementation
+combination
+
+       nPr         n!
+nCr = ----- = ----------
+       r!      r!(n-r)!
+
 '''
 
 import time
 import itertools
 
-def forLoop():
-	l = [1, 2, 3, 4, 5]
+def forLoop(l):
 	output = []
 	for i in range(0, len(l)):
 		for j in range(i+1, len(l)):
 			output.append( (l[i], l[j]) )
-	print output
+	return output
 
-def recursion():
-	l = [1, 2, 3, 4, 5]
-	n = 2
-	def combination(l, n):
-		if n == 0:
-			return [()]
-		r = []
-		for i, v in enumerate(l):
-			for j in combination(l[i+1:], n-1):
-				r.append( (v,) + j )
-		#print '\t' * n, n, 'of', l, '=', r
-		return r
-	print combination(l, n)
+def recursion(l, n):
+	if n == 0:
+		return [()]
+	output = []
+	for i, v in enumerate(l):
+		for j in recursion(l[i+1:], n-1):
+			output.append( (v,) + j )
+	#print '\t' * n, n, 'of', l, '=', output
+	return output
 
-def itertool():
-	l = [1, 2, 3, 4, 5]
-	n = 2
-	print list(itertools.combinations(l, n))
+def itertool(l, n):
+	return list(itertools.combinations(l, n))
 
 if __name__ == '__main__':
 
+	l = [1, 2, 3, 4, 5]
+	n = 2
+
 	s = time.time()
-	forLoop()
+	print forLoop(l)
 	e = time.time()
 	print "elapsed : ", e-s
 
 	s = time.time()
-	recursion()
+	print recursion(l, n)
 	e = time.time()
 	print "elapsed : ", e-s
 
 	s = time.time()
-	itertool()
+	print itertool(l, n)
 	e = time.time()
 	print "elapsed : ", e-s
 
